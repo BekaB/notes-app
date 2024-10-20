922d9b71eb006d1aca167837ee3ff0d210b84f14
BekaB <negash.bereket.se@gmail.com>
2024-10-20 14:51:33 +0400

    many modules

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const chalk = require('chalk')
 const validator = require('validator')
 const message = require('./notes.js')
 
 console.log(message())
 
 console.log(validator.isEmail('ehak@gmail.com'))
-
+console.log(chalk.green.bgGreen('Life is Green '+ chalk.blue.bold.bgRed(' Red Bg with bold text ')))
 
 
 
