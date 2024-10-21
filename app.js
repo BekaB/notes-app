5acf01ab7dd809f5d572e608da112c2176ecf048
BekaB <negash.bereket.se@gmail.com>
2024-10-22 01:41:50 +0400

    raising my hope

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
+// const { describe } = require('yargs')
 
 const command = process.argv[2]
 
-if (command === 'add'){
-    console.log('adding note!')
-} else if (command === 'remove'){
-    console.log('Removing nort!')
-}
+// if (command === 'add'){
+//     console.log('adding note!')
+// } else if (command === 'remove'){
+//     console.log('Removing note!')
+// }
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'adding a note',
+    handler: function(){
+        console.log('adding a note with args')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function(){
+        console.log('removing with args')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'this is the list',
+    handler: function(){
+        console.log('this is the list args')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'this is reading part ',
+    handler: function(){
+        console.log('i am reading with args')
+    }
+})
 
 
 
-
-
+console.log(yargs.argv)
 
 
 
